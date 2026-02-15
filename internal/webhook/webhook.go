b47de3c9e42b08ed1ead38cf11cd51c6ce935b36
buckelew <buckelew.02@gmail.com>
2026-02-15 03:19:59 -0800

    webhook package

--- internal/webhook/webhook.go
+++ internal/webhook/webhook.go
@@ -1,6 +1,38 @@
 package webhook
 
-type Webhook interface {
-	ID() string
-	SendDiscord() // context? product? product will be product type specific
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var client = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
+func Send(webhookURL string, payload any) error {
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		return fmt.Errorf("webhook failed with status: %d", resp.StatusCode)
+	}
+
+	return nil
 }
